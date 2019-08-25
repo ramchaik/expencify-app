2e9ce42adcde24ef077d73c4f98378db8dcb1ab5
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-08-25 10:02:44 +0530

    cleaning

--- src/app.js
+++ src/app.js
@@ -3,18 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-  constructor() {
-    this.name = 'Vaibhav';
-  }
-}
-
-class NewSyntax {
-  name = 'VaibhavFromNew';
-}
-
-const newSyntax = new NewSyntax();
-console.log(newSyntax);
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax);
