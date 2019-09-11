04c07028e8186cbb1f55605c9e6deb8074a35513
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-11 08:18:33 +0530

    BrowserRouter and router set up

--- src/app.js
+++ src/app.js
@@ -4,4 +4,23 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<p>This is my new app.</p>, document.getElementById('app'));
+const ExpenseDashbordPage = () => <div>This is a ExpenseDashbordPage.</div>;
+
+const AddExpensePage = () => <div>This is a create expense page.</div>;
+
+const EditExpensePage = () => <div>This is edit expense page.</div>;
+
+const HelpPage = () => <div>This is Help page</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path={'/'} component={ExpenseDashbordPage} />
+      <Route path={'/create'} component={AddExpensePage} />
+      <Route path={'/edit'} component={EditExpensePage} />
+      <Route path={'/help'} component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
