65a5864827e14a5d3fa903a8f2400521ac4bfc6e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-11 07:39:56 +0530

    intial commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my new app.</p>, document.getElementById('app'));
