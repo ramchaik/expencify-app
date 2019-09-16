df5f19a9b0f18e307fee8565543864f53de279ef
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-16 21:36:35 +0530

    some refactoring

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ExpenseDashbordPage from '../components/ExpenseDashbordPage';
 import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
@@ -14,7 +13,6 @@ const AppRouter = () => (
       <Switch>
         <Route exact={true} path={'/'} component={ExpenseDashbordPage} />
         <Route path={'/create'} component={AddExpensePage} />
-        <Route path={'/edit'} component={EditExpensePage} />
         <Route path={'/help'} component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
