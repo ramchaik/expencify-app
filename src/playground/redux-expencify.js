2b8575fb890e83867317ab38fdb67ec926520044
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-09-28 10:14:50 +0530

    es6 spread operater way

--- src/playground/redux-expencify.js
+++ src/playground/redux-expencify.js
@@ -27,7 +27,10 @@ const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_EXPENSE':
-      return state.concat(action.expense);
+      return [
+        ...state, 
+        action.expense
+      ];
     default:
       return state;
   }
