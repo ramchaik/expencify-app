ad51136d293e516a729c3bf9f16163943b9ac863
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-10-02 12:58:11 +0530

    made a much scalable file structure for redux

---
+++ src/actions/expenses.js
@@ -0,0 +1,31 @@
+import uuid from 'uuid';
+
+// ADD_EXPENSE
+export const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0
+}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+// Remove Expense
+export const removeExpense = ({ id = '' }) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT EXPENSE
+export const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('wate'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
