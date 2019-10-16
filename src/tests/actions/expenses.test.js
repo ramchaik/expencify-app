77e31349f9ef1b255d2c1ed5b88923d6229aeeb9
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2019-10-16 23:04:33 +0530

    added jest frame work and added test cases for actions and selectors

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,58 @@
+import {
+	addExpense,
+	editExpense,
+	removeExpense
+} from '../../actions/expenses';
+import uuid from 'uuid';
+
+test('should setup remove expense action object', () => {
+	const action = removeExpense({ id: '123abc' });
+	expect(action).toEqual({
+		type: 'REMOVE_EXPENSE',
+		id: '123abc'
+	});
+});
+
+test('should setup edit expense action object', () => {
+	const action = editExpense('123abc', {
+		note: 'New note value'
+	});
+	expect(action).toEqual({
+		type: 'EDIT_EXPENSE',
+		id: '123abc',
+		updates: {
+			note: 'New note value'
+		}
+	});
+});
+
+test('should setup add expense action object with provided values', () => {
+	const expenseData = {
+		description: 'Wifi Bill',
+		note: 'Pay fast',
+		amount: 3,
+		createdAt: 10
+	};
+	const action = addExpense(expenseData);
+	expect(action).toEqual({
+		type: 'ADD_EXPENSE',
+		expense: {
+			...expenseData,
+			id: expect.any(String)
+		}
+	});
+});
+
+test('should setup add expense action object without provided values', () => {
+	const action = addExpense();
+	expect(action).toEqual({
+		type: 'ADD_EXPENSE',
+		expense: {
+			id: expect.any(String),
+			description: '',
+			note: '',
+			amount: 0,
+			createdAt: 0
+		}
+	});
+});
---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,53 @@
+import moment from 'moment';
+import {
+	setStartDate,
+	setEndDate,
+	sortByAmount,
+	sortByDate,
+	setTextFilter
+} from '../../actions/filters';
+
+test('should generate set start date object', () => {
+	const action = setStartDate(moment(0));
+	expect(action).toEqual({
+		type: 'SET_START_DATE',
+		startDate: moment(0)
+	});
+});
+
+test('should generate set end date object', () => {
+	const action = setEndDate(moment(0));
+	expect(action).toEqual({
+		type: 'SET_END_DATE',
+		endDate: moment(0)
+	});
+});
+
+test('should generate sort by date object', () => {
+	const action = sortByDate();
+	expect(action).toEqual({ type: 'SORT_BY_DATE' });
+});
+
+test('should generate sort by amount object', () => {
+	const action = sortByAmount();
+	expect(action).toEqual({ type: 'SORT_BY_AMOUNT' });
+});
+
+test('should generate set text filter object with text value', () => {
+	const text = 'Search Check!';
+	const action = setTextFilter(text);
+	expect(action).toEqual({
+		type: 'SET_TEXT_FILTER',
+		text
+	});
+});
+
+test('should generate set text filter object with default', () => {
+	const action = setTextFilter();
+	expect(action).toEqual({
+		type: 'SET_TEXT_FILTER',
+		text: ''
+	});
+});
+
+
