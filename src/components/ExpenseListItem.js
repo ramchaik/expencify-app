008b60ba30217be4bd084edbb5daa30f8bf0cc31
Ramchaik <0vaibhavsingh0@gmail.com>
2019-12-19 23:10:07 +0530

    format time

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({ id, description, createdAt, amount }) => (
-	<div>
-		<Link to={`/edit/${id}`}>
-			<h3>{description}</h3>
-		</Link>
-		<p>
-			{amount} - {createdAt}
-		</p>
-	</div>
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3> {description} </h3>{' '}
+    </Link>{' '}
+    <p>
+      {' '}
+      {amount} - {moment(createdAt).format('MMMM Do YYYY')}{' '}
+    </p>{' '}
+  </div>
 );
 
 export default ExpenseListItem;
