adc0dc02dbf075dadcf4178e8fb24f68a3571a06
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2020-02-12 22:39:36 +0530

    Styling Expensify: Login, Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,23 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { startLogout } from "../actions/auth";
-import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
+import { Link } from 'react-router-dom';
 
 export const Header = ({ startLogout }) => (
-	<header>
-		<h1>Expencify</h1>
-		<header>
-			<NavLink to='/' exact={true} activeClassName='is-active'>
-				Dashboard
-			</NavLink>
-			<NavLink to='/create' activeClassName='is-active'>
-				Create
-			</NavLink>
-			<NavLink to='/edit' activeClassName='is-active'>
-				Edit
-			</NavLink>
-			<NavLink to='/help' activeClassName='is-active'>
-				Help
-			</NavLink>
-			<button onClick={startLogout}>Logout</button>
-		</header>
-	</header>
+  <header className='header'>
+    <div className='content-container'>
+      <div className='header__content'>
+        <Link className='header__title' to='/' exact={true}>
+          <h1> Expencify </h1>
+        </Link>{' '}
+        <button onClick={startLogout}> Logout </button>{' '}
+      </div>
+    </div>
+  </header>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-	startLogout: () => dispatch(startLogout())
+const mapDispatchToProps = dispatch => ({
+  startLogout: () => dispatch(startLogout())
 });
 
 export default connect(undefined, mapDispatchToProps)(Header);
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,8 +7,15 @@ export class LoginPage extends Component {
     const { startLogin } = this.props;
 
     return (
-      <div>
-        <button onClick={startLogin}> Login </button>{' '}
+      <div className='box-layout'>
+        <div className='box-layout__box'>
+          <h1 className='box-layout__title'> Expensify </h1>{' '}
+          <p> It 's time to get your expenses under control.</p>{' '}
+          <button className='button' onClick={startLogin}>
+            {' '}
+            Login with Google{' '}
+          </button>{' '}
+        </div>{' '}
       </div>
     );
   }
